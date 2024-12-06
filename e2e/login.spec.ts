b2634fad2c3157dd096c12e87523f30c95d1dc0d
te99y <f74106246@gs.ncku.edu.tw>
2024-12-05 19:07:13 -0500

    edited passwd in e2e

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -4,7 +4,7 @@ test('Login page should allow valid login', async ({ page }) => {
   await page.goto(''); // login page
 
   await page.fill('input[name="username"]', 'ece30861defaultadminuser');
-  await page.fill('input[name="password"]', 'correcthorsebatterystaple123(!__+@**(A;DROP TABLE packages;');
+  await page.fill('input[name="password"]', "correcthorsebatterystaple123(!__+@**(A'\"`;DROP TABLE packages;");
   await page.click('button:has-text("Login")');
 
   await expect(page.getByText('Welcome, ece30861defaultadminuser!')).toBeVisible();
